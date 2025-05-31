ba4b20ece094ef1bd7278e3c3251c251d201f9f6
DosoCode <tobe2019@hotmail.com>
2025-05-31 11:04:03 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,25 +1,7 @@
-chrome.tabs.onUpdated.addListener(
-  function(tabId, changeInfo, tab) {
-    // read changeInfo data and do something with it
-    // like send the new url to contentscripts.js
-    console.log("Something has changed!")
-    if (changeInfo.url) {
-      chrome.tabs.sendMessage( tabId, {
-        message: 'hello!',
-        url: changeInfo.url
-      })
-    }
-  }
-);
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Extension installed');
+});
 
-//This is a page action. This code adds a rule and checks if the page URL is equal to mail.google.com
-// declarative content lets me enable extension action depending on the page URL
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: 'mail.google.com' },
-        })
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
+chrome.runtime.onStartup.addListener(() => {
+    console.log('Extension started');
 });
