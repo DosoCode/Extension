87a5e5b798fd5de415dd7a4997d4032bd7b6c5c1
DosoCode <tobe2019@hotmail.com>
2025-06-06 00:38:17 +0000

    fixed from local

--- help_popup.js
+++ help_popup.js
@@ -1,3 +1,5 @@
+var is_gmail = false;
+
 document.addEventListener('DOMContentLoaded', () => {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
         const status = document.getElementById('status');
@@ -5,21 +7,40 @@ document.addEventListener('DOMContentLoaded', () => {
             status.textContent = 'No active tab found.';
             return;
         }
+
+
+
         const url = tabs[0].url || '';
         if (url.includes('zoom.com')) {
             status.textContent = 'This tab is a Zoom page!';
         } else if (url.includes('mail.google.com')) {
+            is_gmail = true
             status.textContent = 'This tab is a Gmail page!';
 
         } else {
             status.textContent = 'This tab is NOT a Zoom or Gmail page.';
         }
     });
+    const myButton = document.getElementById('loadTutorial');
+    if (!myButton) {
+        console.error('Button element not found');
+        return;
+    }
+
+    myButton.addEventListener('click', loadGmailHelp);
 });
 
-function loadGmailTutorial () {
-    alert("Load gmail tutorial")
-}
 
-const myButton = document.getElementById('loadTutorial');
-myButton.addEventListener('click', loadGmailTutorial);
+
+function loadGmailHelp() {
+
+    if (is_gmail) {
+        fetch(chrome.runtime.getURL('gmail_tutorial.html'))
+            .then(response => response.text())
+            .then(html => {
+                document.body.innerHTML = html;
+            });
+    } else {
+        alert('This is not a Gmail page. Please open a Gmail tab to view the help.');
+    }
+}
